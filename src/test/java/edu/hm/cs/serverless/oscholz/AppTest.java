d2bc8c226321df8061611402e87435f4497359b7
DieAkuteSense <10849187+DieAkuteSense@users.noreply.github.com>
2020-05-25 19:04:53 +0200

    Modify AppTest

--- src/test/java/edu/hm/cs/serverless/oscholz/AppTest.java
+++ src/test/java/edu/hm/cs/serverless/oscholz/AppTest.java
@@ -10,10 +10,10 @@ public class AppTest {
   public void successfulResponse() {
     App app = new App();
     GatewayResponse result = app.handleRequest(null, null);
-    //assertEquals(result.getStatusCode(), 200);
-    //assertEquals(result.getHeaders().get("Content-Type"), "application/json");
-    //ResponseBody content = result.getBody();
     assertNotNull(result);
-    System.out.println(result.getBody().getVmInfo().toString());
+    assertEquals(result.getStatusCode(), 200);
+    assertEquals(result.getHeaders().get("Content-Type"), "application/json");
+    ResponseBody content = result.getBody();
+    assertNotNull(content);
   }
 }
